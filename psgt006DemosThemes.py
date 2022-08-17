2bd827455e9dc34f9a0d05c7384a3b334da85eff
kcl1s <lohmeyer.keith@gmail.com>
2022-08-17 12:49:17 -0400

    Add files via upload

---
+++ psgt006DemosThemes.py
@@ -0,0 +1,7 @@
+import PySimpleGUI as sg
+
+# https://www.pysimplegui.org/en/latest/#demo-programs
+# https://github.com/PySimpleGUI/PySimpleGUI/tree/master/DemoPrograms
+
+sg.theme_previewer(columns=6, scrollable=True)
+    
